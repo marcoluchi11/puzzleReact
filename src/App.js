e972bed2f2d25a806a3cb4b4d4ded553fbc3978d
Marco Luchi <marcoaluchi@hotmail.com>
2020-07-16 10:37:15 -0300

    se agrego bootstrap y emotion y algunas pruebas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./bootstrap.min.css";
+import styled from "@emotion/styled";
 
+const Prueba = styled.h1`
+  font-family: "Mulish", sans-serif;
+  font-weight: 300;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-8">1</div>
+        <div className="col-4">
+          instrucciones para el rompecabezad Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Voluptate, error.
+        </div>
+      </div>
     </div>
   );
 }
