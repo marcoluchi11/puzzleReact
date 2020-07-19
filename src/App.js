e766af7b04a3470d8cc958e404e5a6c879387072
Marco Luchi <marcoaluchi@hotmail.com>
2020-07-19 10:41:49 -0300

    se pasaron los estados al componente principal y de ahi se pasa a los componentes hijos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./bootstrap.min.css";
 import styled from "@emotion/styled";
 import Instrucciones from "./components/Instrucciones";
@@ -11,14 +11,35 @@ const ContenedorFondo = styled.div`
 `;
 
 function App() {
+  const [seleccion1, setSeleccion1] = useState("");
+  const [seleccion2, setSeleccion2] = useState("");
+  const [imagenes, setImagenes] = useState([]);
+  const [active, setActive] = useState(true);
   return (
     <ContenedorFondo className="container">
       <div className="row">
         <div className="col-md-8">
-          <Puzzle />
+          <Puzzle
+            seleccion1={seleccion1}
+            seleccion2={seleccion2}
+            setSeleccion1={setSeleccion1}
+            setSeleccion2={setSeleccion2}
+            imagenes={imagenes}
+            setImagenes={setImagenes}
+            active={active}
+            setActive={setActive}
+          />
         </div>
-        <div className="col-md-4">
-          <Instrucciones />
+        <div className="col-md-4 ml-0">
+          <Instrucciones
+            seleccion1={seleccion1}
+            seleccion2={seleccion2}
+            setSeleccion1={setSeleccion1}
+            setSeleccion2={setSeleccion2}
+            imagenes={imagenes}
+            setImagenes={setImagenes}
+            setActive={setActive}
+          />
         </div>
       </div>
     </ContenedorFondo>
