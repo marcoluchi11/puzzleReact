93601680fe8069b7fe1bfd3715e72880db19cfb5
Marco <marcoaluchi@hotmail.com>
2020-08-09 21:26:00 -0300

    se agrego componente de Error para validar la respuesta

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Error = () => {
+  return (
+    <p className="alert alert-danger mt-2" role="alert">
+      Error, Ingresa una respuesta!
+    </p>
+  );
+};
+
+export default Error;
