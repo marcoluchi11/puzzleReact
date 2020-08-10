83bf0c0b416dc6745f06ba00b938ebc19215c71f
Marco <marcoaluchi@hotmail.com>
2020-08-10 14:45:08 -0300

    se agrego funcionalidad para ingresar edad en pantalla inicial

--- src/components/PantallaInicial.jsx
+++ src/components/PantallaInicial.jsx
@@ -20,7 +20,10 @@ const Subtitulo = styled.h3`
   font-size: 22px;
   font-weight: 300px;
 `;
-const PantallaInicial = ({ setOpciones, user, setUser }) => {
+const PantallaInicial = ({ setOpciones, user, setUser, setEdad, edad }) => {
+  const handleChange = (e) => {
+    setEdad({ ...edad, [e.target.name]: e.target.value });
+  };
   return (
     <div className="row">
       <div className="col-md-12 d-flex justify-content-center">
@@ -35,6 +38,27 @@ const PantallaInicial = ({ setOpciones, user, setUser }) => {
           datos.
         </small>
       </div>
+      <div className="col-md-4"></div>
+      <div className="col-md-4  text-center">
+        <div class="input-group input-group-sm mb-3 pt-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm">
+              Edad
+            </span>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Small"
+            name="anios"
+            onChange={handleChange}
+            aria-describedby="inputGroup-sizing-sm"
+            placeholder="Ingrese su edad..."
+          />
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+
       <div className="col-md-12 d-flex justify-content-center  mt-4">
         <Subtitulo>Seleccione la dificultad</Subtitulo>
       </div>
