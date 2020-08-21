81a26b15f54c478213c1db13cd35da5ace912f0b
Marco <marcoaluchi@hotmail.com>
2020-08-21 15:13:17 -0300

    se cambio metodo shuffle opr uno mas efectivo

--- src/shuffle.js
+++ src/shuffle.js
@@ -1,7 +1,19 @@
-export function Mezcla(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+export function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
-  return arr;
+
+  return array;
 }
