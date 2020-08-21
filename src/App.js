d69be095977a535c5646f02f91895e675ee1d825
Marco <marcoaluchi@hotmail.com>
2020-08-21 19:07:06 -0300

    se agrega condicional para reflejar la eleccion del usuario

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./bootstrap.min.css";
 import styled from "@emotion/styled";
 import Instrucciones from "./components/Instrucciones";
 //import Puzzle from "./components/Puzzle";
-import ImagenFinal from "./components/ImagenFinal";
+//import ImagenFinal from "./components/ImagenFinal";
 import PantallaInicial from "./components/PantallaInicial";
 import Footer from "./components/Footer";
 import Facil from "./components/Facil";
 import Moderado from "./components/Moderado";
+import Dificil from "./components/Dificil";
 const ContenedorFondo = styled.div`
   background-color: #8ec5fc;
   background-image: linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%);
@@ -23,28 +24,71 @@ function App() {
   const [contador, setContador] = useState(0);
   const [edad, setEdad] = useState({ anios: "" });
   const [error, setError] = useState(false);
+  let eleccion;
+
+  if (opciones === "facil") {
+    eleccion = (
+      <Facil
+        seleccion1={seleccion1}
+        seleccion2={seleccion2}
+        setSeleccion1={setSeleccion1}
+        setSeleccion2={setSeleccion2}
+        imagenes={imagenes}
+        setImagenes={setImagenes}
+        user={user}
+        setUser={setUser}
+        contador={contador}
+        setContador={setContador}
+        edad={edad}
+        error={error}
+        setError={setError}
+      />
+    );
+  }
+  if (opciones === "medio") {
+    eleccion = (
+      <Moderado
+        seleccion1={seleccion1}
+        seleccion2={seleccion2}
+        setSeleccion1={setSeleccion1}
+        setSeleccion2={setSeleccion2}
+        imagenes={imagenes}
+        setImagenes={setImagenes}
+        user={user}
+        setUser={setUser}
+        contador={contador}
+        setContador={setContador}
+        edad={edad}
+        error={error}
+        setError={setError}
+      />
+    );
+  }
+  if (opciones === "dificil") {
+    eleccion = (
+      <Dificil
+        seleccion1={seleccion1}
+        seleccion2={seleccion2}
+        setSeleccion1={setSeleccion1}
+        setSeleccion2={setSeleccion2}
+        imagenes={imagenes}
+        setImagenes={setImagenes}
+        user={user}
+        setUser={setUser}
+        contador={contador}
+        setContador={setContador}
+        edad={edad}
+        error={error}
+        setError={setError}
+      />
+    );
+  }
   return (
     <ContenedorFondo className="container">
       {opciones ? (
         // ROMPECABEZAS
         <div className="row">
-          <div className="col-md-8">
-            <Moderado
-              seleccion1={seleccion1}
-              seleccion2={seleccion2}
-              setSeleccion1={setSeleccion1}
-              setSeleccion2={setSeleccion2}
-              imagenes={imagenes}
-              setImagenes={setImagenes}
-              user={user}
-              setUser={setUser}
-              contador={contador}
-              setContador={setContador}
-              edad={edad}
-              error={error}
-              setError={setError}
-            />
-          </div>
+          <div className="col-md-8">{eleccion}</div>
           <div className="col-md-4 ml-0">
             <Instrucciones
               user={user}
@@ -57,7 +101,6 @@ function App() {
               setOpciones={setOpciones}
               setEdad={setEdad}
             />
-            <ImagenFinal contador={contador} />
           </div>
         </div>
       ) : (
@@ -81,13 +124,14 @@ export default App;
 
 // cUANDO HACES CLICK EN LA DIFICULTAD QUE TE APAREZCA UNOS SEGUNDOS LA IMAGEN COMPLETA.
 // VER POR QUE A VECES NO ME MUESTRA CARTEL GANADOR
-// VER COMO HACER PARA ALTERNAR ENTRE COMPONENTES DE DIFICULTAD
-// ELIMINAR PIKACHU DE LA VISTA PREVIA
+
 // AVERIGUAR SI UNA PREGUNTA POR PIEZA SIGUE SIENDO VALIDA EN EL DE 27 Y/O 18
+// DIFICULTAD DIFICIL HACER MAS CHICAS LAS PIEZAS PARA QUE QUEPA 5X5 Y NO 4X5
 
 // SECCION PREGUNTAS MAS FACHERA -- Listo
 // LOGUEO O OPCION ANONIMO -- Listo
 // EDAD NOMBRE MAIL -- Listo
+// VER COMO HACER PARA ALTERNAR ENTRE COMPONENTES DE DIFICULTAD -- Listo
 //AGREGAR INPUT PARA SABER LA EDAD -- Listo
 // explicar motivo de registro -- Listo
 // MANDAR A OTRO DOCUMENTO DISTINTO, PARA QUE VEAN LAS RTAS // FORO DE RESPUESTAS -- Listo
