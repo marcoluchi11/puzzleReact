7e2b3f42e4d8c58648fe02e39d751dc58f837611
Marco <marcoaluchi@hotmail.com>
2020-08-21 20:21:22 -0300

    se agrega imagen resuelta al completar las preguntas

--- src/components/ImagenFinal.jsx
+++ src/components/ImagenFinal.jsx
@@ -1,24 +1,54 @@
-import React, { useState, useEffect } from "react";
-import imagenFinal from "./../images/imagenfinal.png";
-import styled from "@emotion/styled";
+import React, { useState, useEffect, Fragment } from "react";
 
+import styled from "@emotion/styled";
+import dificil from "./../images/imagenesFinales/dificil25.jpg";
+import medio from "./../images/imagenesFinales/moderado16.jpg";
+import facil from "./../images/imagenesFinales/facil12.jpg";
 const Imagen = styled.img`
   width: 220px;
   height: 220px;
 `;
-const ImagenFinal = ({ contador }) => {
+const ImagenFinal = ({ contador, opciones }) => {
   const [verImagen, setVerImagen] = useState(false);
+  let cant;
+  // eslint-disable-next-line
+  switch (opciones) {
+    case "facil":
+      cant = 12;
+      break;
+    case "medio":
+      cant = 16;
+      break;
+    case "dificil":
+      cant = 25;
+      break;
+  }
   useEffect(() => {
-    if (contador === 8) {
+    if (contador === cant) {
       setVerImagen(true);
       setTimeout(() => {
         setVerImagen(false);
-      }, 6500);
+      }, 8000);
     }
-  }, [contador]);
-
+  }, [contador, cant]);
+  let imagenFinal;
+  // eslint-disable-next-line
+  switch (cant) {
+    case 12:
+      imagenFinal = facil;
+      break;
+    case 16:
+      imagenFinal = medio;
+      break;
+    case 25:
+      imagenFinal = dificil;
+      break;
+  }
   const imagen = verImagen ? (
-    <Imagen className="mt-3 mb-3" src={imagenFinal} alt="imagen final" />
+    <Fragment>
+      <small>Imagen resuelta</small>
+      <Imagen className="mt-3 mb-3" src={imagenFinal} alt="imagen final" />
+    </Fragment>
   ) : null;
   return imagen;
 };
