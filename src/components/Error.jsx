e15d74cc29cc1ca2adca87682294d0004c02ffdf
Marco <marcoaluchi@hotmail.com>
2020-08-22 11:48:52 -0300

    se agrego logo de error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import ErrorIcon from "./../images/error-24px.svg";
 const Error = ({ mensaje }) => {
   return (
     <p className="alert alert-danger mt-2" role="alert">
+      <img className="mr-3" src={ErrorIcon} alt="" />
       {mensaje}
     </p>
   );
