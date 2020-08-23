40d917b00169556d1d830156b31f1cbbdff9b7b2
Marco <marcoaluchi@hotmail.com>
2020-08-23 15:08:00 -0300

    se agrego icono de tick

--- src/components/Cartel.jsx
+++ src/components/Cartel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
+import tick from "./../images/check_circle_outline-24px.svg";
 const DivCartel = styled.div`
   margin-top: 3rem;
   font-weight: 700;
@@ -8,10 +9,10 @@ const DivCartel = styled.div`
 const Cartel = () => {
   const url =
     "https://docs.google.com/spreadsheets/d/1lcnrmA9kCN78yr4aVS2VU6c7-9Z7zZ5PxnsBMRrS1Uo/edit?usp=sharing";
-  // if (cartel === null) return;
   return (
     <DivCartel className="alert alert-success" role="alert">
-      <p>Felicitaciones, completaste el rompecabezas!!</p>
+      <img src={tick} alt="Correcto" />
+      <p>Felicitaciones, completaste el rompecabezas!</p>
       <p>
         Puedes ver las respuestas de los demas{" "}
         <a href={url} rel="noopener noreferrer" target="_blank">
