9d443ef82e8106e63b2b276c872a77c4a7d013ad
Marco <marcoaluchi@hotmail.com>
2020-11-09 14:01:37 -0300

    se pasa contador para mostrar en e lcartel

--- src/components/Cartel.jsx
+++ src/components/Cartel.jsx
@@ -6,13 +6,14 @@ const DivCartel = styled.div`
   margin-top: 3rem;
   font-weight: 700;
 `;
-const Cartel = () => {
+const Cartel = ({ contadorRtas }) => {
   const url =
     "https://docs.google.com/spreadsheets/d/1lcnrmA9kCN78yr4aVS2VU6c7-9Z7zZ5PxnsBMRrS1Uo/edit?usp=sharing";
   return (
     <DivCartel className="alert alert-success" role="alert">
       <img src={tick} alt="Correcto" />
       <p>Felicitaciones, completaste el rompecabezas!</p>
+      <p>Cantidad de Respuestas correctas: {contadorRtas}/8</p>
       <p>
         Puedes ver las respuestas de los demas{" "}
         <a href={url} rel="noopener noreferrer" target="_blank">
