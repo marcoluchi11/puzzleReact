c4d617ff0719a2dea80936626b7d37f37335b7c9
Marco <marcoaluchi@hotmail.com>
2020-11-20 17:51:08 -0300

    se agrego ranking ordenado top 5

---
+++ src/components/TablaRanking.jsx
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import { nanoid } from "nanoid";
+const TablaRanking = ({ ranking }) => {
+  return (
+    <Fragment>
+      <h4>Ranking</h4>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Tiempo</th>
+            <th>Respuestas</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ranking.map((rank, index) => {
+            if (index > 4) return null;
+
+            return (
+              <tr key={nanoid()}>
+                <th>{index + 1}</th>
+                <td>{rank[0]}</td>
+                <td>{rank[1]}</td>
+                <td>{rank[2]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default TablaRanking;
